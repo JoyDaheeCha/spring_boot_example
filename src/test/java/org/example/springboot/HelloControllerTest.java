30edf6d1845e2f3a5508fa87d428beb3e58c5d3f
Joy <daheecha0612@gmail.com>
2020-10-29 13:12:22 +0900

    2.4 Convert code in HelloController to code with lombok

--- src/test/java/org/example/springboot/HelloControllerTest.java
+++ src/test/java/org/example/springboot/HelloControllerTest.java
@@ -1,4 +1,5 @@
 package org.example.springboot;
+
 import org.example.springboot.web.HelloController;
 
 import org.junit.Test;
@@ -10,8 +11,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class) /*스프링 부트테스트와 JUnit 사이 연결자 역할*/
 @WebMvcTest(controllers = HelloController.class)
@@ -28,5 +29,21 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())  /*status 200 유무 체크*/
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+
     }
 }
